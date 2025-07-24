0e429b8c2a03c57f7d7ffd9a6dc9c23287df5d79
Bulat Idrisov <33020309+idrisovbu@users.noreply.github.com>
2025-07-24 09:36:41 -0700

    Update B1_launcher_two_part_model.R
    Updates parameter csv filename to differentiate between disease_counts script

--- aim1_scripts/B_analysis/B1_launcher/B1_launcher_two_part_model.R
+++ aim1_scripts/B_analysis/B1_launcher/B1_launcher_two_part_model.R
@@ -4,7 +4,6 @@
 ##' Date: 2025-05-04
 ##----------------------------------------------------------------
 
-
 # # Clear environment and set library paths
 rm(list = ls())
 
@@ -29,9 +28,8 @@ if (Sys.info()["sysname"] == 'Linux'){
   l <- 'L:/'
 }
 
-
 # Input location (adjust date as needed based on the run date)
-run_date <- "20250620"
+run_date <- "bested" # bested was 20250620 (update as needed)
 fp_input_data <- file.path(l, "LU_CMS/DEX/hivsud/aim1/A_data_preparation", run_date, "aggregated_by_year")
 
 # List all .parquet files
@@ -48,17 +46,16 @@ df_params <- data.frame(directory = input_files) %>%
   filter(!(year_id %in% c(2002, 2004, 2006)))%>%
   filter(file_type == "RX")  #delete after processing
 
-
 # Write CSV for full job param list
 param_dir <- file.path(l, "LU_CMS/DEX/hivsud/aim1/resources_aim1/")
 dir.create(param_dir, recursive = TRUE, showWarnings = FALSE)
-fp_parameters <- file.path(param_dir, "by_year_parameters_aim1.csv")
+fp_parameters <- file.path(param_dir, "two_part_model_parameters_aim1.csv")
 fwrite(df_params, fp_parameters)
 
 # Define script and output paths
 user <- Sys.info()[["user"]]
 script_path <- file.path(h, "repo/dex_us_county/misc/hivsud/aim1_scripts/B_analysis/B2_worker/B2_worker_two_part_model.R")
-log_dir <- file.path(l, "LU_CMS/DEX/hivsud/aim1/B_analysis/logs", run_date)
+log_dir <- file.path(l, "LU_CMS/DEX/hivsud/aim1/B_analysis/logs")
 dir.create(log_dir, recursive = TRUE, showWarnings = FALSE)
 
 # Submit jobs
@@ -68,9 +65,9 @@ jid <- SUBMIT_ARRAY_JOB(
   args = c(fp_parameters),
   error_dir = log_dir,
   output_dir = log_dir,
-  queue = "all.q", # 256 is longer wait time due to lower nodes but max can be 1024G; max on long is 2048.
+  queue = "long.q", # 256 is longer wait time due to lower nodes but max can be 1024G; max on long is 2048.
   n_jobs = nrow(df_params),
-  memory = "250G", # 750as of May 26th run 750G on allq was ok, but needed more than 16hours time, need to put on long with 3 days at least
+  memory = "1000G", # 750as of May 26th run 750G on allq was ok, but needed more than 16hours time, need to put on long with 3 days at least
   threads = 1,
   time = "24:00:00", #120:00:00 72 hours max with this all.q; "24:00:00""
   ## long.q is 384H (2 weeks) if its under 72 then do all.q #https://docs.cluster.ihme.washington.edu/#hpc-execution-host-hardware-specifications
