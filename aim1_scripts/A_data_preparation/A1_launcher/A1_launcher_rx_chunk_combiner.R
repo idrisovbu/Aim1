acf72cdba827c9b365e1cae1f4debe4942f53385
Bulat Idrisov <33020309+idrisovbu@users.noreply.github.com>
2025-07-30 21:07:25 -0700

    Update A1_launcher_rx_chunk_combiner.R
    Cleans up chunk combiner

--- aim1_scripts/A_data_preparation/A1_launcher/A1_launcher_rx_chunk_combiner.R
+++ aim1_scripts/A_data_preparation/A1_launcher/A1_launcher_rx_chunk_combiner.R
@@ -1,13 +1,14 @@
 ##----------------------------------------------------------------
 ##' Title: A1_launcher_rx_chunk_combiner.R
-##'
-##' Purpose:
+##' 
+##' Purpose: A1_launcher_rx_data_compiler.R is ran first before this script to generate year/state/age level RX data chunks, after
+##'          which this script combines the chunks into the final year/age level data. 
 ##----------------------------------------------------------------
 
-
-# # Clear environment and set library paths
+##----------------------------------------------------------------
+## 0. Clear environment and set library paths
+##----------------------------------------------------------------
 rm(list = ls())
-
 pacman::p_load(dplyr, openxlsx, RMySQL, data.table, ini, DBI, tidyr, openxlsx)
 library(lbd.loader, lib.loc = sprintf("/share/geospatial/code/geospatial-libraries/lbd.loader-%s", R.version$major))
 if("dex.dbr"%in% (.packages())) detach("package:dex.dbr", unload=TRUE)
@@ -30,14 +31,12 @@ if (Sys.info()["sysname"] == 'Linux'){
   l <- 'L:/'
 }
 
+##----------------------------------------------------------------
+## 1. Generate DF with all processed chunks
+##----------------------------------------------------------------
 
-# Dynamically read in directories where data is stored to see what data they have in the first place
-# Create a table based off their directory hierarchy (use code to scan folder directories to determine available data)
-
-date_of_rx_chunk_input <- "20250620"
-
-#rx_input_data <- paste0("/mnt/share/limited_use/LU_CMS/DEX/hivsud/aim1/A_data_preparation/", date_of_rx_chunk_input, "/rx_chunks")
-
+# List out processed RX Chunks
+date_of_rx_chunk_input <- format(Sys.time(), "%Y%m%d")
 
 rx_input_data_dir <- file.path("/mnt/share/limited_use/LU_CMS/DEX/hivsud/aim1/A_data_preparation/", date_of_rx_chunk_input, "rx_chunks")
 
@@ -48,9 +47,7 @@ rx_input_data <- list.files(
   recursive = TRUE
 )
 
-#rx_input_data <- list.files(rx_input_data, pattern = "\\.parquet$", full.names = TRUE)
-
-
+# Create DF listing all processed RX chunks
 df_rx_list_input_data_subset <- data.frame(directory = rx_input_data) %>%
   mutate(
     year_id = str_extract(directory, "(?<=rx_)\\d{4}") %>% as.numeric(),
@@ -59,9 +56,18 @@ df_rx_list_input_data_subset <- data.frame(directory = rx_input_data) %>%
     state = str_extract(directory, "(?<=_state)[A-Z]{2}")
   )
 
-
+# Check against params file to ensure that all chunks have output files 
 param_output_dir <- "/mnt/share/limited_use/LU_CMS/DEX/hivsud/aim1/resources_pharm/"
+df_A1_rx_parameters_pharm <- fread(file.path(param_output_dir, "/A1_rx_parameters_pharm.csv"))
+if (nrow(A1_rx_parameters_pharm) == nrow(df_rx_list_input_data_subset)) {
+  print("All RX chunks created by A1_launcher_rx_data_compiler.R")
+} else {
+  print("MISSING CHUNKS! COMPARE INPUT_DATA_SUBSET AGAINST A1_RX_PARAMETERS_PHARM.CSV")
+}
 
+##----------------------------------------------------------------
+## 2. Save CSVs with list of RX chunk filepaths & unique combinations for processing
+##----------------------------------------------------------------
 
 # Ensure the directory exists
 dir.create(param_output_dir, recursive = TRUE, showWarnings = FALSE)
@@ -70,39 +76,23 @@ dir.create(param_output_dir, recursive = TRUE, showWarnings = FALSE)
 fp_parameters_full <- file.path(param_output_dir, "parameters_rx_chunks.csv")
 write.csv(df_rx_list_input_data_subset, file = fp_parameters_full, row.names = FALSE)
 
-
 # 2. Create and save unique year-based job list for launcher
-# df_rx_unique_years <- df_rx_list_input_data_subset %>%
-#   select(year_id) %>%
-#   distinct() %>%
-#   arrange(year_id)
-
 df_rx_unique_combos <- df_rx_list_input_data_subset %>%
   select(year_id, age_group_years_start) %>%
   distinct() %>%
   arrange(year_id, age_group_years_start)
 
-
-
 fp_parameters_unique <- file.path(param_output_dir, "chunk_parameters_rx_unique.csv")
-#write.csv(df_rx_unique_years, file = fp_parameters_unique, row.names = FALSE)
 write.csv(df_rx_unique_combos, file = fp_parameters_unique, row.names = FALSE)
 
-
-##########################################
-# Define output directories (Using existing date-stamped folder)
-##########################################
+##----------------------------------------------------------------
+## 3. Define output directories (Using existing date-stamped folder)
+##----------------------------------------------------------------
 user <- Sys.info()[["user"]]
-# Define base output directory (shared with F2T)
-base_output_dir <- "/mnt/share/limited_use/LU_CMS/DEX/hivsud/aim1/A_data_preparation"
-date_folder <- format(Sys.time(), "%Y%m%d")  # Generates folder based on today's date
-output_folder <- file.path(base_output_dir, date_folder)  # Uses the existing date folder
 
-# Define logs directory inside the shared date-stamped output folder (specific for RX)
-#log_dir <- file.path(output_folder, "dx_count_logs_rx")
 # Define output and log directory paths
 date_folder <- format(Sys.Date(), "%Y%m%d")
-log_dir <- file.path("/mnt/share/limited_use/LU_CMS/DEX/hivsud/aim1/A_data_preparation/logs", date_folder)
+log_dir <- file.path("/mnt/share/limited_use/LU_CMS/DEX/hivsud/aim1/A_data_preparation/logs/")
 
 # Function to ensure the logs directory exists
 ensure_dir_exists <- function(dir_path) {
@@ -117,6 +107,9 @@ ensure_dir_exists(log_dir)
 # Update script path to new organized structure
 script_path <- paste0(h, "/repo/dex_us_county/misc/hivsud/aim1_scripts/A_data_preparation/A2_worker/A2_worker_rx_chunks_combiner.R")
 
+##----------------------------------------------------------------
+## 4. Submit Jobs
+##----------------------------------------------------------------
 
 # Submit jobs to the cluster - Launcher Script for Aim 1 study - All Years, Ages, TOC from fp_parameter
 jid <- SUBMIT_ARRAY_JOB(
@@ -127,12 +120,10 @@ jid <- SUBMIT_ARRAY_JOB(
   output_dir = log_dir,
   queue = "all.q",
   n_jobs = nrow(df_rx_unique_combos),
-  #n_jobs = nrow(df_rx_unique_years),
-  #n_jobs = 5, #just for testing purposes
-  memory = "250G", #determine based off running a few individual runner scripts and checking the Rstudio environment to see how much memory it uses
-  threads = 1, # can usually keep at 1 unless the code itself is doing something complicated
-  time = "72:00:00", # determine based off running a runner scripts and timing the entire script to get an estimate, then add +15 min
+  memory = "250G",
+  threads = 1, 
+  time = "5:00:00", 
   user_email = paste0(user, "@uw.edu"),
   archive = FALSE,
-  test = F # In R, T = TRUE
+  test = F # F = Full Run, T = Test Run (only run the first job in a batch)
 )
